9d4a6b5b1962ef3e99c87935ccbd9d41b5edaf8c
@coder shruti <74962507+ShrutiRani7029@users.noreply.github.com>
2023-09-23 19:53:28 +0530

    variables

--- Basics_01/script.js
+++ Basics_01/script.js
@@ -1,1 +1,22 @@
 console.log('Basics');
+
+//variables, constants, comments
+// :- double slash represents comment
+// var: redeclare + reassign (less in use), let: reassign only(more in use), const: noreassign + noredeclare (prefer to use)
+
+var score=34;
+//console.log(score);
+var score=56;
+console.log(score);
+
+let points =89;
+//let points=67; -> noredeclaration
+points=78; // but reassign can be possible
+console.log(points);
+
+const value=87;
+//value=90; no redeclaration + no assign
+console.log(value);
+
+
+
